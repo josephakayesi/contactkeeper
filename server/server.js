4d04ef2bee38ab2357e666272ebb9446e02ba7bc
Joseph Akayesi <josephakayesi@gmail.com>
2019-10-22 14:20:13 +0000

    Prepare for deployment. Test 11 : Change express static directory back to client/build

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use('/api/auth', Route.getRoute().auth)
 // Server static assets in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('../client/build'))
+    app.use(express.static('client/build'))
 
     app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build/index.html'))
